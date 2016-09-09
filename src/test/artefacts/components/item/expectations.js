0eec181591bb73642fb08b23fe360d7c16dadbe2
Kristian Mandrup <kmandrup@gmail.com>
2016-09-09 15:34:28 +0200

    fix expectations for component-item route test

--- src/test/artefacts/components/item/expectations.js
+++ src/test/artefacts/components/item/expectations.js
@@ -2,6 +2,6 @@ var chai = require('chai'),
     expect = chai.expect;
 
 module.exports = function(res) {
-  console.log('RESPONSE', res);
-  expect(res.length).to.equal(2);
+  console.log('RESPONSE', res.body);
+  expect(res.body.name).to.equal('contacts');
 }
