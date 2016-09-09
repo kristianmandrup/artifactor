11c6ff703af5ef19152804afd8a440de879ec26a
Kristian Mandrup <kmandrup@gmail.com>
2016-09-09 15:01:15 +0200

    alternative simpler component route

--- src/routes/component.js
+++ src/routes/component.js
@@ -1,8 +1,11 @@
 const item = require('./entity/item');
 
-router.get('/components/:id', async function (ctx, next) {
-  await item(ctx);
-  await ctx.render('index');
-  next();
-})
+// router.get('/components/:id', async function (ctx, next) {
+//   await item(ctx);
+//   await ctx.render('index');
+//   next();
+// })
+
+router.get('/components/:id', item)
+
 module.exports = router;
