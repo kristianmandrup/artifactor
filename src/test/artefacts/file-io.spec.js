c8e0c3b9da61e33b10a1222f5073e4fdd0db52a5
Kristian Mandrup <kmandrup@gmail.com>
2016-09-09 14:35:58 +0200

    add version test

--- src/test/artefacts/file-io.spec.js
+++ src/test/artefacts/file-io.spec.js
@@ -9,21 +9,29 @@ describe('Io', () => {
   const components = fileIo.create('components', name);
 
   describe('#item', () => {            
-    it('should return item file content', (done) => {
-      components.item().then(res => {   
-        // console.log('ITEM', res);
-        _.expect(res).to.match(/"name": "contacts"/);
-        done();
-      }).catch(err => {
-        console.error(err);
-      })
+    it('should return item file content', async () => {
+      let content = await components.item();
+      // console.log('CONTENT', content);
+      let json = JSON.parse(content);   
+      _.expect(json.name).to.equal('contacts');
+      _.expect(json.versions.length).to.equal(2);
+    });
+  });
+
+  describe('#version', () => {            
+    it('should return item file content', async () => {
+      let content = await components.version();
+      // console.log('CONTENT', content);
+      let json = JSON.parse(content);   
+      _.expect(json.name).to.equal('contacts');
+      _.expect(json.version).to.equal('1.2');
     });
   });
 
   describe('#list', () => {            
     it('should return list file content with 3 items, first item contacts', async () => {
       let content = await components.list();
-      console.log('CONTENT', content);
+      // console.log('CONTENT', content);
       let json = JSON.parse(content);
       _.expect(json.length).to.equal(3);   
       _.expect(json[0].name).to.equal('contacts');                
