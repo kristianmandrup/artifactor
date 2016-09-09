781a019e9a8706dc0944907495b153186942a51b
Kristian Mandrup <kmandrup@gmail.com>
2016-09-09 08:32:23 +0200

    better call-api

--- src/test/artefacts/components/item/expectations.js
+++ src/test/artefacts/components/item/expectations.js
@@ -2,5 +2,6 @@ var chai = require('chai'),
     expect = chai.expect;
 
 module.exports = function(res) {
+  console.log('RESPONSE', res);
   expect(res.length).to.equal(2);
 }
--- src/test/utils/call-api.js
+++ src/test/utils/call-api.js
@@ -1,12 +1,13 @@
-module.exports = function (path, method = 'GET') {
-  return function get() {
-    return agent(method, path)
-      .end()
-      .then(res => {
-        return res; 
-      }, (err) => {
-        //err.response has the response from the server
-        throw 'Unable to retrieve list of apps'; 
-      });
-  } 
+const agent = require('./agent');
+
+module.exports = async function(path, method = 'GET') {
+  return agent(method, path)
+    .end()
+    .then(res => {
+      return res; 
+    }, (err) => {
+      //err.response has the response from the server
+      console.error('Agent error', err);
+      throw 'Unable to retrieve list of apps'; 
+    }); 
 }
